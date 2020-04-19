c0f73faaeb204371a507e2d7314b27ff7949fa28
mitakoss <mitakoss@gmail.com>
2020-04-19 16:54:50 +0300

    first commit

---
+++ src/serverapp/ServerThread.java
@@ -0,0 +1,69 @@
+package serverapp;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+public class ServerThread extends Thread{
+    
+    //create variables of server form
+    ServerForm sf;
+    
+    //variables to read and write messages
+    ObjectInputStream oin;
+    ObjectOutputStream out;
+    
+    //server socket object to create server
+    ServerSocket serverSocket;
+    
+    //socket object to create socket connection
+    Socket socket;
+    
+    //will pass parameter ServerForm object
+    public ServerThread(ServerForm sf){
+        this.sf = sf;
+        
+        try{
+            //create server object with specific port number
+            serverSocket = new ServerSocket(Settings.port);
+            JOptionPane.showMessageDialog(sf, "Server Started");
+            start();
+            
+        }catch(Exception e){
+    }
+    }
+    
+    //create a method to send a message
+    public void sendMessage(String msg){
+        try{
+            out.writeObject(msg);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+        public void run(){
+           while(true){
+               try{
+                   //accepting incoming connections to server using thread
+                  socket = serverSocket.accept();
+                  //calling method to create input/output objects
+                  openReader();
+               }catch(Exception e){
+                   e.printStackTrace();
+               }
+           }
+        }
+
+    private void openReader() {
+        try{
+            oin = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
